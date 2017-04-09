af922e7eb392b49d3138286ce78d1c56cc5c6909
Frank Garcia <futboler18@yahoo.com>
2017-04-09 12:54:48 -0700

    base of tower defense project, most major systems implemented, will be a work in progress

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Node : MonoBehaviour {
+
+    public Color hoverColor;
+    public Color notEnoughMoneyColor;
+    public Vector3 positionOffset;
+
+    [Header("Optional")]
+    public GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    BuildManager buildManager;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+
+        buildManager = BuildManager.instance;
+    }
+
+    public Vector3 GetBuildPosition()
+    {
+        return transform.position + positionOffset;
+    }
+
+    private void OnMouseDown()
+    {
+        // Checking if the shop ui is over gameobjects and if so not allowing use of gameobjects
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (!buildManager.CanBuild)
+            return;
+
+        if (turret != null)
+        {
+            Debug.Log("Can't build there! - TODO: Display on screen.");
+            return;
+        }
+
+        buildManager.BuildTurretOn(this);
+    }
+
+    private void OnMouseEnter()
+    {
+        // Checking if the shop ui is over gameobjects and if so not allowing use of gameobjects
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (!buildManager.CanBuild)
+            return;
+
+        if (buildManager.HasMoney)
+        {
+            rend.material.color = hoverColor;
+        }
+        else
+        {
+            rend.material.color = notEnoughMoneyColor;
+        }
+
+        
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
